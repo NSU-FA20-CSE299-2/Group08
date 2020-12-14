30afcab1ac67756edc39e1549032f852ba2fbce4
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2020-12-14 15:05:43 +0600

    Enhancement #5: Added the base for the user registration page
    Enhancement #5 : Base structure added. Form contents are yet to be added.
    Page name: user_reg.php

---
+++ Code/Project Code/user_reg.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>User Reg. </title>
+	<link rel="stylesheet" href="css/admindesign.css">
+	<style>
+	body{
+		line-height: 1.6;
+		font-size: 120%;
+	}
+	select{
+		padding: 6px;
+		border-radius: 5px;
+		margin-bottom: 2px;
+
+	}
+	button{
+		height: 35px;
+		border-radius: 5px;
+
+	}
+	input{
+		background-color: rgb(236, 112, 99);
+
+	}
+	</style>
+</head>
+<body>
+ <h1> Register User</h1>
+
+</body>
+</html>
