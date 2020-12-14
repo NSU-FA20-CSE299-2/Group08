d63ce2a71006441352baed31c96fe5dd5ec9e78a
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2020-12-14 15:09:54 +0600

    Enhancement #5: Added form contents for user registration page
    Enhancement #5: Added form contents for user registration..
    Admin will  register new user based on these information.

--- Code/Project Code/user_reg.php
+++ Code/Project Code/user_reg.php
@@ -28,6 +28,19 @@
 </head>
 <body>
  <h1> Register User</h1>
+ <form action="insert_user.php" method="post">
+  <label>User ID: <input name="user_id" required></label><br/>
+  <label>Name: <input type="text" name="user_name" required></label><br/>
+	<label>Password: <input type="password" name="user_password" required></label><br/>
+  <label>Instructor ID:<input name="instructor_id" required></label><br/>
+  <label>Email: <input type="email" name="user_email" required></label><br/>
+  <label>Phone:  <input name="user_phone" required></label><br/>
+  <label> Address: <input type="text" name="user_address"></label><br/>
+  <label>Age: <input name="user_age"></label><br/>
 
+
+
+ <button type="submit" name="submit">Submit</button><br/>
+</form>
 </body>
 </html>
