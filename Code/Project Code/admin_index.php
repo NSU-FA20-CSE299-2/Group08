231d339bcf64785398290baf4ecd4178fc2ff7e8
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2021-01-07 22:56:19 +0600

    Enhancement #18 : Added option to quick view monthly payment status

--- Code/Project Code/admin_index.php
+++ Code/Project Code/admin_index.php
@@ -47,4 +47,23 @@ $row=mysqli_fetch_assoc($get);
 
 
       <footer>
-      
+      <form action="insert_trx.php?id='.$row['admin_id'].'" method="post">
+      <label>Check Monthly Transactions: <input type="month" name="month" required><button type="submit" name="submit">View</button></form>
+
+
+
+       <form action="insert_user_payment.php" method="post">
+       <label>Check Quick Payment  Status: <input name="user_id" placeholder="Enter user id.." required></label><button type="submit" name="submit">Submit</button></form>
+
+
+<a id="adminlogout" href="userlogin.php">Logout</a> </footer>
+
+
+   <!-- Optional JavaScript -->
+   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+ </body>
+</html>'
+?>
