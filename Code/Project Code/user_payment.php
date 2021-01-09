6bf50e71fe0621e62955530c1acb79a6df4ad908
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2021-01-09 22:47:56 +0600

    Enhancement #22 : Created user payment page.
    Enhancement #22 : Page name 'user_payment.php'

---
+++ Code/Project Code/user_payment.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>User Payment </title>
+	<link rel="stylesheet" href="css/admindesign.css">
+
+</head>
+<body style="margin: 5%">
+ <h1> User Payment </h1>
+ <form action="insert_user_payment.php" method="post">
+  <label>User ID: <input name="user_id" required></label><br/>
+  Package ID: <select name="package_id">
+  <option value="7"  selected>Weekly</option><br/>
+  <option value="30" >Monthly</option><br/>
+  <option value="180" >Half-Yearly</option><br/>
+  <option value="365" >Yearly</option><br/>
+</select><br/>
+  <label>Amount: <input name="amount" required></label><br/>
+    <label> Payment Date: <input type="date" name="payment_date" required></label><br/>
+
+ <button type="submit" name="submit">Submit</button><br/>
+</form>
+</body>
+</html>
