27dd6b80c8f9d55e2e3e046407732311a12c8a6e
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2021-01-11 21:04:50 +0600

    Enhancement #23 : Created page to view user payment status.
    Enhancement #23 : Page name 'view_paymentstatus.php'

---
+++ Code/Project Code/view_paymentstatus.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/admindesign.css">
+<style>
+td:nth-child(2){
+  background-color:rgba(26, 82, 118,.3);
+
+
+}
+</style>
+
+    <title>View Records</title>
+  </head>
+  <body>
+    <header>
+    <h1>All Payment Records</h1>
+<?php
+require 'connection.php';
