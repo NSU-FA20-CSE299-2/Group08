45a09c586de953a5fd2657f4b2eb459a1d07ea03
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2021-01-16 19:44:21 +0600

    Enhancement #30 : Added php code to insert to database

--- Code/Project Code/insert_alluser_routine.php
+++ Code/Project Code/insert_alluser_routine.php
@@ -15,4 +15,24 @@ if(isset($_POST['user_id'])&&isset($_POST['routine_day'])&&isset($_POST['exc_nam
   $exc_name_5=!empty($exc_name_5)?"'$exc_name_5'" : "NULL";
   $sql="insert into total_userroutine(user_id,routine_day,exc_name_1,exc_name_2,exc_name_3,exc_name_4,exc_name_5,sets,reps) values('$user_id','$routine_day','$exc_name_1','$exc_name_2','$exc_name_3',$exc_name_4,$exc_name_5,'$sets','$reps')
   ON DUPLICATE KEY UPDATE
-  
+  exc_name_1='$exc_name_1',
+exc_name_2='$exc_name_2',
+exc_name_3='$exc_name_3',
+exc_name_4=$exc_name_4,
+exc_name_5=$exc_name_5,
+sets='$sets',
+reps='$reps'  ";
+$insert_all_routine=mysqli_query($conn,$sql);
+if($insert_all_routine)
+{
+  echo "success";
+}
+else {
+  echo "error in loop: ".mysqli_error($conn);
+}
+}
+else
+{
+echo "failed to insert";
+}
+?>
