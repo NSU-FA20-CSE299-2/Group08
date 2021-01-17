6cb6f246b7a7bd1d8cd274377a72fc680bdef9c4
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2021-01-17 20:05:49 +0600

    Enhancement #38 : Added form contents to take client measurement

--- Code/Project Code/measurement.php
+++ Code/Project Code/measurement.php
@@ -27,4 +27,19 @@
 	</style>
 </head>
 <body>
-  
+  <h1> Take User Body Measurement </h1>
+ <form action="insert_bodymeasurement.php" method="post">
+  <label>User ID: <input name="user_id" required></label><br/>
+  Height:<br/>
+  <label> Feet: <input type="number" name="feet" min="4" max="7" required></label>
+  <label> Inches: <input type="number" name="inches" min="0" max="12" required></label><br/>
+  Weight:<br/>
+  <label> Kg: <input type="text" name="kg" required></label>
+  <label> gm: <input type="text" name="gm"  value="0" required></label><br/>
+	<label>Date: <input type="date" name="meas_date" required></label><br/>
+
+
+ <button type="submit" name="submit">Submit</button><br/>
+</form>
+</body>
+</html>
