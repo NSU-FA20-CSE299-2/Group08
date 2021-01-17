80ba187217c4bd133817616310d5c968c0d0019e
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2021-01-17 21:18:40 +0600

    Enhancement #43 : Added tables to display data

--- Code/Project Code/myview_userprofile.php
+++ Code/Project Code/myview_userprofile.php
@@ -16,4 +16,29 @@ echo  '
 <table>
   <tr> <th>Fields </th>
           <th>Values </th>
-          
+          </tr>
+   <tr><td>Id: </td>
+        <td>'.$row['user_id'].'</td></tr>
+        <tr><td>Name: </td>
+             <td>'.$row['user_name'].'</td></tr>
+             <tr><td>Password: </td>
+                  <td>'.$row['user_password'].'</td></tr>
+                  <tr><td>Trainer : </td>
+             <td>'.$row['trainer_name'].'</td></tr>
+         <tr><td>Email: </td>
+        <td>'.$row['user_email'].'</td></tr>
+            <tr><td>Phone: </td>
+           <td>'.$row['user_phone'].'</td></tr>
+       <tr><td>Address: </td>
+            <td>'.$row['user_address'].'</td></tr>
+      <tr><td>Age: </td>
+     <td>'.$row['user_age'].'</td></tr><td>Gender: </td>
+  <td>'.$row['user_gender'].'</td></tr>
+    <tr><td>Joined on: </td>
+    <td>'.$row['join_date'].'</td></tr>
+               <tr><td>Package ID: </td>
+  <td>'.$row['package_id'].'</td></tr></table>';
+
+
+echo '
+<div id="editdiv"><a id="edit" href="edit_userprofile.php?id='.$row['user_id'].'">EDIT</a></div></body>'; ?>
