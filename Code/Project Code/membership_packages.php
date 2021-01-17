7653c5a35467b3ef092fd72f1cd4db7997c982d0
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2021-01-17 20:19:04 +0600

    Enhancement #39  : Created admin page to edit membership packages

---
+++ Code/Project Code/membership_packages.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Membership Packages</title>
+		<link rel="stylesheet" href="css/admindesign.css">
+		<style>
+		body{
+			line-height: 1.6;
+			font-size: 120%;
+		}
+		select{
+			padding: 6px;
+			border-radius: 5px;
+			margin-bottom: 2px;
+
+		}
+		button{
+			height: 35px;
+			border-radius: 5px;
+
+		}
+		input{
+			background-color: rgb(236, 112, 99);
+
+		}
+		</style>
+</head>
+<body>
