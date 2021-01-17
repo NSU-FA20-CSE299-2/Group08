a0f11bab15e5d12accf01ca38a47b36f382b5789
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2021-01-17 21:28:25 +0600

    Enhancement #44 : Added php to retrieve workout routine from database

--- Code/Project Code/myview_workoutroutine.php
+++ Code/Project Code/myview_workoutroutine.php
@@ -10,3 +10,33 @@ echo  '
 </head>
 <title>Workout Routine</title>
 <body>
+<table>
+  <tr> <th> Day </th>
+          <th> Exercise 1</th>
+          <th> Exercise 2 </th>
+          <th> Exercise 3</th>
+          <th> Exercise 4</th>
+          <th> Exercise 5 </th>
+          <th> Sets </th>
+          <th> Reps </th>
+  </tr>';
+while($row=mysqli_fetch_assoc($get))
+{
+
+    echo '<tr>
+            <td>'.$row['routine_day'].'</td>
+            <td>'.$row['exc_name_1'].'</td>
+            <td>'.$row['exc_name_2'].'</td>
+            <td>'.$row['exc_name_3'].'</td>
+              <td>'.$row['exc_name_4'].'</td>
+                <td>'.$row['exc_name_5'].'</td>
+                <td>'.$row['sets'].'</td>
+                <td>'.$row['reps'].'</td>';
+}}
+else
+{
+  echo "no routine has been assigned yet";
+}
+
+
+ ?>
