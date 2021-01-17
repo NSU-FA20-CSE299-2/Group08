502d1b8e057a8133aaa4c981a2630c748bc708d2
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2021-01-17 22:07:10 +0600

    Enhancement #46 : Created admin page to view all client body status

---
+++ Code/Project Code/viewall_bodymeasurement.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/admindesign.css">
+
+    <title>View All Body Measurement</title>
+  </head>
+  <body>
+    <header>
+    <h1>User Body Status</h1>
+<?php
+require 'connection.php';
