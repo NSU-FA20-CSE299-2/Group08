268659d1d1461ecfa351bc574ba392538c29ee84
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2021-01-17 12:30:31 +0600

    Enhancement #34 : Added php content

--- Code/Project Code/insert_trainer.php
+++ Code/Project Code/insert_trainer.php
@@ -9,3 +9,18 @@ if(isset($_POST['trainer_id'])&&isset($_POST['trainer_name'])&&isset($_POST['tra
   $trainer_gender=$_POST['trainer_gender'];
   $trainer_salary=$_POST['trainer_salary'];
   $hire_date=$_POST['hire_date'];
+  $sql="insert into trainer(trainer_id,trainer_name,trainer_email,trainer_phone,trainer_address,trainer_gender,trainer_salary,hire_date) values('$trainer_id','$trainer_name','$trainer_email','$trainer_phone','$trainer_address','$trainer_gender','$trainer_salary','$hire_date')";
+  $insert_trainer_data=mysqli_query($conn,$sql);
+  if($insert_trainer_data)
+  {
+    echo "Trainer Data inserted";
+  }
+  else{
+    echo "Data Value contradicts";
+  }
+}
+else {
+  echo "Data insertion FAILED";
+}
+
+ ?>
