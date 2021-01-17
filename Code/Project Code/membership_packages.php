dfb791deb4e684316bb14685585c322774ebde8d
JushrafRahman <55083404+JushrafRahman@users.noreply.github.com>
2021-01-17 20:20:10 +0600

    Enhancement #39 : Added form input labels

--- Code/Project Code/membership_packages.php
+++ Code/Project Code/membership_packages.php
@@ -27,3 +27,16 @@
 		</style>
 </head>
 <body>
+  <h1> Edit Packages </h1>
+ <form action="insert_mem_pack.php" method="post">
+    Package ID: <br/>
+   <label> <input type="radio" name="package_id" value="7"> Weekly</label><br/>
+   <label> <input type="radio" name="package_id" value="30"> Monthly</label><br/>
+   <label> <input type="radio" name="package_id" value="180"> Half Yearly</label><br/>
+   <label> <input type="radio" name="package_id" value="365"> Yearly</label><br/>
+ <label>Package Amount: <input name="package_amount" required></label><br/>
+ <label> Package Name:  <input type="text" name="package_name" required></label><br/>
+ <button type="submit" name="submit">Submit</button><br/>
+</form>
+</body>
+</html>
